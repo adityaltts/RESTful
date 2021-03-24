e1a846548b008a76dc40cfb4a96715e31f406486
adityaltts <78849471+adityaltts@users.noreply.github.com>
2021-03-24 17:32:18 +0530

    commit

---
+++ jpa-hibernate-one-to-many-mysql-rest/src/test/com/hellokoding/jpa/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hellokoding.jpa;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
